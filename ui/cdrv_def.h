8e32af8501ee69dcb0ccec147beb504e11e99cd7
visualfc <visualfc@gmail.com>
2016-02-05 21:03:50 +0800

    update

---
+++ ui/cdrv_def.h
@@ -0,0 +1,39 @@
+// Copyright 2015-2016 visualfc <visualfc@gmail.com>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#ifndef _CDRV_DEF_H_
+#define _CDRV_DEF_H_
+
+#include <stdbool.h>
+
+typedef struct {
+    const char *data;
+    int size;
+} string_head;
+
+typedef struct {
+    const char *data;
+    int size;
+} byte_array_head;
+
+typedef struct {
+	const bool *data;
+	int	size;
+} bool_array_head;
+
+typedef struct {
+	const int *data;
+	int	size;
+} int_array_head;
+
+typedef void* pvoid;
+
+typedef struct {
+	const pvoid *data;
+	int	 size;
+} ptr_array_head;
+
+#define pvoid_size sizeof(pvoid)
+
+#endif //_CDRV_DEF_H_
